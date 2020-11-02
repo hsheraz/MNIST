02655c3f8ac2dd420f5b06324b6be268f35c2780
Leems <45567279+hsheraz@users.noreply.github.com>
2020-11-01 23:12:47 -0500

    Delete Data_analysis.py

--- Data_analysis.py
+++
@@ -1,7 +0,0 @@
-import sys
-import win32com.client
-
-xlApp = win32com.client.Dispatch("Excel.Application")
-print("Excel library version:", xlApp.Version)
-filename,password = 'C:\myfiles\foo.xls', 'qwerty12'
-xlwb = xlApp.Workbooks.Open(filename, Password=password)
